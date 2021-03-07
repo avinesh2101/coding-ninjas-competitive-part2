a71f3952b05dea8fa56fb85046535f70199122d4
Avinesh <avineshpandey2101@gmail.com>
2021-03-07 16:18:11 +0530

    7march

---
+++ 13DP-1/boredom.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+int solve(int n,vector<int>A)
+{
+    int *freq=new int[1005];
+    int *dp=new int[1005];
+    
+    for(int i=0;i<1005;i++)
+    {
+        freq[i]=dp[i]=0;
+    }
+    // memset(freq, 0, sizeof(freq));
+    // memset(dp, 0, sizeof(dp));
+    
+    for(int i=0;i<n;i++)
+        freq[A[i]]++;
+    
+    dp[1]=freq[1];
+    
+    for(int i=2;i<=1000;i++)
+    {
+        dp[i]=max(dp[i-1],dp[i-2]+i*freq[i]);
+    }
+    int maximum=dp[1000];
+    delete[] freq;
+    delete[] dp;
+    return maximum;
+}
